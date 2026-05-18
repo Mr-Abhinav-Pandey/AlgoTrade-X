e9f7103d63584c1559a7a45525643a99f8131828
Mr-Abhinav-Pandey <abhinavp1205@gmail.com>
2026-05-18 19:51:55 +0530

    feat: Add WebSocket infrastructure skeleton (Phase 1)

---
+++ src/websocket_feed.cpp
@@ -0,0 +1,12 @@
+#include "websocket_feed.h"
+#include <iostream>
+
+// TODO: Implement real WebSocket connection
+// Requires: Boost.Beast, Boost.Asio, OpenSSL
+// Status: Infrastructure placeholder
+
+void run_websocket_feed_test()
+{
+    std::cout << "[TODO] WebSocket feed not yet implemented" << std::endl;
+    std::cout << "Waiting for: Boost + OpenSSL installation" << std::endl;
+}
---
+++ src/websocket_feed.h
@@ -0,0 +1,9 @@
+#ifndef WEBSOCKET_FEED_H
+#define WEBSOCKET_FEED_H
+
+// Real-time Binance WebSocket streaming
+// Phase 1: Raw message printer (no parsing yet)
+
+void run_websocket_feed_test();
+
+#endif // WEBSOCKET_FEED_H
